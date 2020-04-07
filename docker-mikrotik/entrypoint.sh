ff856750ec63201b7c6b6b37e468fa81dc5bb011
Federico Pereira <lord.basex@gmail.com>
2020-04-07 15:05:17 -0300

    Update entrypoint.sh

--- docker-mikrotik/entrypoint.sh
+++ docker-mikrotik/entrypoint.sh
@@ -6,4 +6,5 @@ qemu-system-x86_64 \
     -m 256 \
     -hda $ROUTEROS_IMAGE \
     -device e1000,netdev=net0 \
+    -k $KEYBOARD \
     -netdev user,id=net0,hostfwd=tcp::21-:21,hostfwd=tcp::22-:22,hostfwd=tcp::23-:23,hostfwd=tcp::80-:80,hostfwd=tcp::443-:443,hostfwd=tcp::500-:500,hostfwd=tcp::1194-:1194,hostfwd=tcp::1701-:1701,hostfwd=tcp::1723-:1723,hostfwd=tcp::4500-:4500,hostfwd=tcp::8291-:8291,hostfwd=tcp::8728-:8728,hostfwd=tcp::8729-:8729
