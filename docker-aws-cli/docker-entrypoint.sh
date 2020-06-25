aef073d0bdc194dc0757ebeeeb0f7408b85e627c
Federico Pereira <lord.basex@gmail.com>
2020-06-25 17:23:53 -0300

    Update docker-entrypoint.sh

--- docker-aws-cli/docker-entrypoint.sh
+++ docker-aws-cli/docker-entrypoint.sh
@@ -8,5 +8,27 @@ if [ -z "$(ls -A /var/spool/cron/crontabs )" ]; then
   cp -fra /usr/src/crontabs/* /var/spool/cron/crontabs
 fi
 
+if [ $MSMTP = "true" ]; then
+
+cat > /etc/msmtprc <<ENDLINE
+defaults
+auth           ${MSMTP_AUTH}
+tls            ${MSMTP_TTS}
+tls_trust_file ${MSMTP_TTS_TRUST_FILE}
+syslog         ${MSMTP_SYSLOG}
+account        $MSMTP_ACCOUNT
+host           ${MSMTP_HOST}
+auth           ${MSMTP_ACCOUNT_AUTH}
+port           ${MSMTP_PORT}
+from           ${MSMTP_FROM}
+user           ${MSMTP_USER}
+password       ${MSMTP_PASSWORD}
+# Set a default account
+account default : $MSMTP_ACCOUNT
+aliases        /etc/aliases
+ENDLINE
+
+fi
+
 chmod -R 777 /scripts/*
 chmod -R 0600 /var/spool/cron/crontabs/*
