eeef4a9d648afdf714ae2c039c4df498a44d6877
Federico Pereira <lord.basex@gmail.com>
2020-06-25 20:01:14 -0300

    Update restoremysqldump.sh

--- docker-aws-cli/scripts/restoremysqldump.sh
+++ docker-aws-cli/scripts/restoremysqldump.sh
@@ -3,12 +3,12 @@ cd /tmp
 
 if [ "$1" != "" ]; then
     echo "Restore backup $1"
-    aws s3 cp s3://${AWS_S3_PATH}${1} . 
-    if [ "$?" -ne 0  ]; then 
+    aws s3 cp s3://${AWS_S3_SQL}/${1} .
+    if [ "$?" -ne 0  ]; then
 	echo "ERROR 404 FILE NO EXISTE"
     	exit 1
     else
-	echo 'DOWNLOAD OK `date +"%m-%d-%Y %H_%M_%S"` ' 
+	echo 'DOWNLOAD OK `date +"%m-%d-%Y %H_%M_%S"` '
     	echo "mysql -uroot -p$MYSQL_ROOT_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE < $1"
     	echo "rm -fr $1"
     	mysql -uroot -p$MYSQL_ROOT_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE < $1
