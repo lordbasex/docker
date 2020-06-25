f6c7b43bb0639ef96dc7ff9c4676a46fad5dbe89
Federico Pereira <lord.basex@gmail.com>
2020-06-25 20:11:18 -0300

    Update mysqldump.sh

--- docker-aws-cli/scripts/mysqldump.sh
+++ docker-aws-cli/scripts/mysqldump.sh
@@ -5,5 +5,7 @@ FILE_DB=db_`date +"%m-%d-%Y_%H_%M_%S"`.sql
 /usr/bin/mysqldump -uroot -p$MYSQL_ROOT_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE > $FILE_DB
 echo "aws s3api put-object --bucket ${AWS_S3_SQL} --key ${FILE_DB} --tagging 's3=sql' --body ${FILE_DB}" >> /var/log/mysqldump.log
 aws s3api put-object --bucket ${AWS_S3_SQL} --key ${FILE_DB} --tagging 's3=sql' --body ${FILE_DB} >> /var/log/mysqldump.log
-echo -e "Subject: Auto Backup Crontab mysqldump `date +'%m-%d-%Y %H:%M:%S'`\n`cat /var/log/mysqldump.log `" | msmtp -a default ${MAIL_NOTIFICATION}
+if [ $MSMTP = "true" ]; then
+  echo -e "Subject: Auto Backup Crontab mysqldump `date +'%m-%d-%Y %H:%M:%S'`\n`cat /var/log/mysqldump.log `" | msmtp -a default ${MAIL_NOTIFICATION}
+fi
 rm -fr $FILE_DB
