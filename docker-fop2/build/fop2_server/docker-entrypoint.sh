d11539f45d6e283db9844dc7bfc3cc0f20d91e6e
Federico Pereira <lord.basex@gmail.com>
2021-02-10 11:11:39 -0300

    mod fop2

--- docker-fop2/build/fop2_server/docker-entrypoint.sh
+++ docker-fop2/build/fop2_server/docker-entrypoint.sh
@@ -3,9 +3,10 @@ set -e
 
 if [ -z "$(ls -A /usr/local/fop2 )" ]; then
   mkdir -p /usr/local/fop2
-  cp -fra /usr/src/fop2/server/* /usr/local/fop2/
+  cp -fra  /usr/local/fop2.org/* /usr/local/fop2/
+fi
 
-echo "Configuration file fop2.cfg"
+echo "FOP2 - Configuration file fop2.cfg"
 sed -i "s/\(manager_host*\)\(.*\)/\1=$FOP2_AMI_HOST/" /usr/local/fop2/fop2.cfg
 sed -i "s/\(manager_port*\)\(.*\)/\1=$FOP2_AMI_PORT/" /usr/local/fop2/fop2.cfg
 sed -i "s/\(manager_user*\)\(.*\)/\1=$FOP2_AMI_USER/" /usr/local/fop2/fop2.cfg 
@@ -15,9 +16,9 @@ sed 's|ssl_certificate_file=/etc/pki/tls/certs/localhost.crt|ssl_certificate_fil
 cat /tmp/fop2.cfg > /usr/local/fop2/fop2.cfg
 sed 's|ssl_certificate_key_file=/etc/pki/tls/private/localhost.key|ssl_certificate_key_file=/etc/letsencrypt/live/certificate/privkey.pem|'  "/usr/local/fop2/fop2.cfg" > /tmp/fop2.cfg
 cat /tmp/fop2.cfg > /usr/local/fop2/fop2.cfg
+touch /usr/local/fop2/buttons_custom.cfg
 echo "plugin=phone:/var/www/html/admin/plugins/phone" >> /usr/local/fop2/fop2.cfg
 echo "buttonfile=buttons_custom.cfg" >> /usr/local/fop2/fop2.cfg
-echo "buttonfile=buttons_custom_webrtc.cfg" >> /usr/local/fop2/fop2.cfg
 echo "buttonfile=buttons_custom_trunk.cfg" >> /usr/local/fop2/fop2.cfg
 
 cat > /usr/local/fop2/buttons_custom_trunk.cfg  <<ENDLINE
@@ -35,6 +36,6 @@ ENDLINE
 
 rm -fr /tmp/fop2.cfg
 
-fi
+echo "FOP2 - RUN"
 
-/usr/bin/supervisord
+exec "$@"
