6b835ac1402594bfebec3fafca21d174a55e9ad7
root <Federico Pereira>
2023-01-01 22:15:59 -0300

    mkt new version

--- docker-mikrotik/README.md
+++ docker-mikrotik/README.md
@@ -1,13 +1,9 @@
 # Mikrotik RouterOS in Docker
 
-Lord BaseX (c) 2014-2020
+Lord BaseX (c) 2014-2022
  Federico Pereira <fpereira@cnsoluciones.com>
 
 This docker image is made for laboratory use and testing only. We are not responsible if you use it for commercial purposes or if you use it in production.
-
-[![VIDEO](https://raw.githubusercontent.com/lordbasex/docker/master/docker-mikrotik/docker-mikrotik-index.png)](https://www.youtube.com/watch?v=Nx5BJHA-F-M)
-
-## How to use
 
 
 ### Use image from docker hub
@@ -16,81 +12,26 @@ This docker image is made for laboratory use and testing only. We are not respon
 docker pull cnsoluciones/mikrotik
 ```
 
-```bash
-docker run  --name=mkt01 -tid -p 121:21 -p 122:22 -p 123:23 -p 150:50 -p 151:51 -p 180:80 -p 1443:443 -p 1500:500 -p 11194:1194 -p 11701:1701 -p 11723:1723 -p 14500:4500 -p 15900:5900 -p 18080:8080 -p 18291:8291 -p 18728:8728 -p 18729:8729 cnsoluciones/mikrotik:latest
-```
-
 
 ### Use in docker-compose.yml
 
 Example is [here](docker-compose.yml).
 
 ```yml
-version: "3.3"
+version: "3"
 
 services:
-
-  mkt01:
-    image: cnsoluciones/mikrotik:latest
-    container_name: mkt01
-    restart: always
-    ports:
-      - "121:21"
-      - "122:22"
-      - "123:23"
-      - "150:50"
-      - "151:51"
-      - "180:80"
-      - "1443:443"
-      - "1500:500"
-      - "11194:1194"
-      - "11701:1701"
-      - "11723:1723"
-      - "14500:4500"
-      - "15900:5900"
-      - "18080:8080"
-      - "18291:8291"
-      - "18728:8728"
-      - "18729:8729"
-    environment:
-      - "VNCPASSWORD=false"
-    network_mode: bridge
-    privileged: true
-
-
-  winbox:
-    image: cnsoluciones/novnc-winbox:latest
-    container_name: winbox
-    hostname: winbox
-    restart: always
-    #volumes:
-    #  - ./user-data/.wine:/home/alpine/.wine
-    links:
-      - "mkt01"
-    ports:
-      - "5901:5900"
-      - "18081:8080"
-    network_mode: bridge
+    routeros:
+        container_name: "mkt"
+        image: cnsoluciones/mikrotik:6.49.7
+        privileged: true
+        ports:
+            - "8291:8291"
+            - "8729:8729"
+            - "8728:8728"
+            - "2222:22"
+        cap_add:
+            - NET_ADMIN
+        devices:
+            - /dev/net/tun
 ```
-
-## List of exposed ports
-
-For access via VNC: 5900 Or [noVNC http://localhost:18080/](http://localhost:18080/) And [Winbox http://localhost:18081/](http://localhost:18081/)
-
-Default ports of RouterOS: 21, 22, 23, 80, 443, 8291, 8728, 8729
-
-IPSec: 50, 51, 500, 4500
-
-OpenVPN: 1194
-
-L2TP: 1701
-
-PPTP: 1723
-
-# noVNC Password
-
-* Change variable VNCPASSWORD=true
-* docker exec mkt01 cli changepassword mypassword
-
-## Github
-Visit my github where I have several projects with Docker [CNSoluciones](https://github.com/lordbasex/Docker)
---
+++ docker-mikrotik/bin/entrypoint.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+# A bridge of this name will be created to host the TAP interface created for
+# the VM
+QEMU_BRIDGE='qemubr0'
+
+# DHCPD must have an IP address to run, but that address doesn't have to
+# be valid. This is the dummy address dhcpd is configured to use.
+DUMMY_DHCPD_IP='10.0.0.254'
+
+# These scripts configure/deconfigure the VM interface on the bridge.
+QEMU_IFUP='/routeros/bin/qemu-ifup'
+QEMU_IFDOWN='/routeros/bin/qemu-ifdown'
+
+# The name of the dhcpd config file we make
+DHCPD_CONF_FILE='/routeros/dhcpd.conf'
+
+function default_intf() {
+    ip -json route show | jq -r '.[] | select(.dst == "default") | .dev'
+}
+
+# First step, we run the things that need to happen before we start mucking
+# with the interfaces. We start by generating the DHCPD config file based
+# on our current address/routes. We "steal" the container's IP, and lease
+# it to the VM once it starts up.
+/routeros/bin/generate-dhcpd-conf.py $QEMU_BRIDGE > $DHCPD_CONF_FILE
+default_dev=`default_intf`
+
+# Now we start modifying the networking configuration. First we clear out
+# the IP address of the default device (will also have the side-effect of
+# removing the default route)
+ip addr flush dev $default_dev
+
+# Next, we create our bridge, and add our container interface to it.
+ip link add $QEMU_BRIDGE type bridge
+ip link set dev $default_dev master $QEMU_BRIDGE
+
+# Then, we toggle the interface and the bridge to make sure everything is up
+# and running.
+ip link set dev $default_dev up
+ip link set dev $QEMU_BRIDGE up
+
+touch /var/lib/udhcpd/udhcpd.leases
+# Finally, start our DHCPD server
+udhcpd -I $DUMMY_DHCPD_IP -f $DHCPD_CONF_FILE &
+
+# And run the VM! A brief explanation of the options here:
+# -enable-kvm: Use KVM for this VM (much faster for our case).
+# -nographic: disable SDL graphics.
+# -serial mon:stdio: use "monitored stdio" as our serial output.
+# -nic: Use a TAP interface with our custom up/down scripts.
+# -drive: The VM image we're booting.
+exec qemu-system-x86_64 \
+    -nographic -serial mon:stdio \
+    -m 256 \
+    "$@" \
+    -hda $ROUTEROS_IMAGE \
+    -device e1000,netdev=net0 \
+    -netdev user,id=net0 \
+    -device e1000,netdev=net1 \
+    -netdev user,id=net1 \
+    -device e1000,netdev=net2 \
+    -netdev user,id=net2 \
+    -device e1000,netdev=net3 \
+    -netdev user,id=net3 \
+    -device e1000,netdev=net4 \
+    -netdev user,id=net4 \
+    -device e1000,netdev=net5 \
+    -netdev user,id=net5 \
+    -device e1000,netdev=net6 \
+    -netdev user,id=net6 \
+    -device e1000,netdev=net7 \
+    -netdev user,id=net7 \
+    -device e1000,netdev=net8 \
+    -netdev user,id=net8 \
+    -device e1000,netdev=net9 \
+    -netdev user,id=net9 \
+    -device e1000,netdev=net10 \
+    -netdev user,id=net10,hostfwd=tcp::21-:21,hostfwd=tcp::22-:22,hostfwd=tcp::23-:23,hostfwd=tcp::80-:80,hostfwd=tcp::443-:443,hostfwd=tcp::8291-:8291,hostfwd=tcp::8728-:8728,hostfwd=tcp::8729-:8729,hostfwd=tcp::1194-:1194,hostfwd=tcp::1701-:1701,hostfwd=tcp::1723-:1723 \
+    -nic tap,id=qemu0,script=$QEMU_IFUP,downscript=$QEMU_IFDOWN 
