633bf349726569545552d3a8639ee4bb327279ca
iPERFEX TEAM <info@iperfex.com>
2025-08-19 10:24:35 -0300

    Update entrypoint.sh

--- docker-comfyui/entrypoint.sh
+++ docker-comfyui/entrypoint.sh
@@ -15,9 +15,10 @@ fi
 
 # Asegurar directorios (evita PermissionError del nodo Load3D)
 mkdir -p /app/comfyui/input/3d /app/comfyui/output /app/comfyui/models
+mkdir -p /app/comfyui/models/checkpoints /app/comfyui/models/clip /app/comfyui/models/clip_vision /app/comfyui/models/configs /app/comfyui/models/controlnet /app/comfyui/models/diffusers /app/comfyui/models/diffusion_models /app/comfyui/models/embeddings /app/comfyui/models/gligen /app/comfyui/models/hypernetworks /app/comfyui/models/loras /app/comfyui/models/photomaker /app/comfyui/models/style_models /app/comfyui/models/text_encoders /app/comfyui/models/unet /app/comfyui/models/upscale_models /app/comfyui/models/vae /app/comfyui/models/vae_approx
 
 # (Opcional) forzar permisos si el volumen viene como root
-# chown -R 1000:1000 /app/comfyui || true
+chown -R 1000:1000 /app/comfyui || true
 
 cd /app/comfyui
 
