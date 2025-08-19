b768bca0a3cfad2dac6542b31c5f593565241c67
iPERFEX TEAM <info@iperfex.com>
2025-08-19 12:43:43 -0300

    fix

--- docker-comfyui/example-models.sh
+++ docker-comfyui/example-models.sh
@@ -5,7 +5,7 @@ wget https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/sp
 wget https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors -O vae/wan_2.1_vae.safetensors
 wget https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors -O diffusion_models/wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors
 wget https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors -O diffusion_models/wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors
-wget https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_t2v_lightx2v_4steps_lora_v1.1_high_noise.safetensors -O loras/wan2.2_t2v_lightx2v_4steps_lora_v1.1_high_noise.safetensor
+wget https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_t2v_lightx2v_4steps_lora_v1.1_high_noise.safetensors -O loras/wan2.2_t2v_lightx2v_4steps_lora_v1.1_high_noise.safetensors
 wget https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_t2v_lightx2v_4steps_lora_v1.1_low_noise.safetensors -O loras/wan2.2_t2v_lightx2v_4steps_lora_v1.1_low_noise.safetensors
 cd -
 chown -R 1000:1000 ./user-data
