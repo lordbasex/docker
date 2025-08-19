ef138e73754291c012d7a59da3321922f243dc33
iPERFEX TEAM <info@iperfex.com>
2025-08-19 10:28:21 -0300

    Update tool.sh

--- docker-comfyui/tool.sh
+++ docker-comfyui/tool.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
 # Create folders and set permissions
-mkdir -p ./user-data/models ./user-data/input ./user-data/output
+mkdir -p ./user-data/input ./user-data/output
+mkdir -p ./user-data/models/checkpoints ./user-data/models/clip ./user-data/models/clip_vision ./user-data/models/configs ./user-data/models/controlnet ./user-data/models/diffusers ./user-data/models/diffusion_models ./user-data/models/embeddings ./user-data/models/gligen ./user-data/models/hypernetworks ./user-data/models/loras ./user-data/models/photomaker ./user-data/models/style_models ./user-data/models/text_encoders ./user-data/models/unet ./user-data/models/upscale_models ./user-data/models/vae ./user-data/models/vae_approx
 chown -R 1000:1000 ./user-data
